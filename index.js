7aaf5a66562624a1db1805514612a08a89998adc
secretuser23-games <williamdimond47@gmail.com>
2026-09-24 09:19:38 -0400

    Version X.0: All buttons work now, going up and down the tree works, and clicking on a correct.exe deletes the file, but still gives the reward.

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ function gebid(input){
 }
 
 function log(what){
+    if(what == null){
+        return
+    }
     var out = document.createElement("p");
     out.innerHTML = what;
     gebid("logs")?.appendChild(out);
+    return out
 }
-
+function clearLog(){
+    gebid("logs").innerHTML = "Logs: "
+}
 var globalIdCounter = 0; 
 var levels =[0], specs = [`4 MB RAM`, `33 Mhz CPU`, `Integrated GPU`, `9" CRT Screen` ];
 document.cookie = "levels=" + JSON.stringify(levels) + "; path=/;";
@@ -53,17 +59,24 @@ function showPowerOptions(){ closeAllMenus(); if(gebid("powerMenu")) gebid("powe
 var maxForBar = 0, currentProgress = 0;
 var activeTree = null;
 var currentFolderID = -1;
-
+var tree = [];
 function startGame(gameID){
+    gebid("gameMenu").classList = "closedMenu"
     gebid("fileExplorer").className = "fileExplorer";
     currentFolderID = -1;
     currentProgress = 0;
-    
-    var tree;
-    if(gameID == 3 || gameID == 6 || gameID == 9) { maxForBar = 4; tree = createTree(8); }
-    else if(gameID == 2 || gameID == 5 || gameID == 8) { maxForBar = 3; tree = createTree(7); }
+    if(gameID == 7 || gameID == 8 || gameID == 9) { maxForBar = 4; tree = createTree(8); }
+    else if(gameID == 4 || gameID == 5 || gameID == 6) { maxForBar = 3; tree = createTree(7); }
     else { maxForBar = 2; tree = createTree(6); }
-    
+    if(gameID == 2){
+        startSpeedrun(120)
+    }
+    else if(gameID == 5){
+        startSpeedrun(60)
+    }
+    else if(gameID == 8){
+        startSpeedrun(30)
+    }
     activeTree = tree;
     for(var i = 0; i < maxForBar; i++){
         let folderNode = null;
@@ -82,8 +95,10 @@ function startGame(gameID){
                 inside: null,
                 depth: folderNode.depth + 1,
                 id: globalIdCounter++,
-                fileSize: getRandomNumber(100) + 1
+                fileSize: getRandomNumber(100) + 1,
+                parentID: folderNode.id
             });
+            log(folderNode.id)
         }
     }
     for(var j = 0; j < (tree[0].inside.length * 2); j++){
@@ -103,7 +118,8 @@ function startGame(gameID){
                 inside: null,
                 depth: folderNode.depth + 1,
                 id: globalIdCounter++,
-                fileSize: getRandomNumber(100) + 1
+                fileSize: getRandomNumber(100) + 1,
+                parentID: folderNode.id
             });
         }
     }
@@ -120,17 +136,29 @@ function openFile(id){
         if(gebid("progressToWin")) {
             gebid("progressToWin").value = 100 * (1 / maxForBar) * currentProgress;
         }
-        log("Found System Helper Link!");
         if(currentProgress === maxForBar){
             if(typeof win === "function") win();
-            else log("You Win! (win() function triggered)");
         }
+        deleteFile(node.id)
     } else {
         log("Opened file: " + node.Name);
     }
 }
-
+function deleteFile(id) {
+    var whatToDel = findNodeById(tree, id);
+    if (!whatToDel) return;
+    var parentIdForLater = whatToDel.parentID;
+    var parentNode = findNodeById(tree, parentIdForLater);
+    if (parentNode && parentNode.inside) {
+        var index = parentNode.inside.findIndex(item => item.id === id);
+        if (index !== -1) {
+            parentNode.inside.splice(index, 1);
+        }
+    }
+    updateFileExplorer(activeTree, parentIdForLater);
+}
 function openFolder(id){
+    currentFolderID = id
     updateFileExplorer(activeTree, id);
 }
 
@@ -153,19 +181,24 @@ function updateFileExplorer(tree, currentID){
         var isExecutable = (isFolder || item.Name.includes(".mp3") || item.Name.includes(".exe")) ? "Yes" : "No";
         var isCorrectSystemFile = (item.Name.toLowerCase() === "correct.exe" || item.Name === "Bonus.exe") ? "Yes" : "IDK, you choose";
         
-        insert += `<tr><td>` +actionButton + `</td><td>` + item.Name + `</td><td>` + item.fileSize + `MB</td><td>${isExecutable}</td><td>${isCorrectSystemFile}</td></tr>`;
+        insert += `<tr><td>` + actionButton + `</td><td>` + item.Name + `</td><td>` + item.fileSize + `MB</td><td>${isExecutable}</td><td>${isCorrectSystemFile}</td><td>${item.id}</td></tr>`;
     }
-    log("frame")
-    if(currentFolderID != -1){
-    insert += "<tr><td> + <button onclick='openFolder(" + findNodeById(tree, currentFolderID).parent.id + ")'>Return up 1 Folder</button></td></tr>";}
+    if(currentFolderID == -1){
+        log("No button to add")
+    }
+    else if(findNodeById(tree,currentFolderID).parentID == -1){
+        insert += "<tr><td><button onclick='openFolder(-1)'>Return To Top</button></td><td></td><td></td><td></td><td></td></tr>";
+    }
+    else{
+        insert += "<tr><td><button onclick='openFolder(" + findNodeById(tree, currentFolderID).parentID + ")'>Return Up 1 Folder</button></td><td></td><td></td><td></td><td></td><td></td></tr>";
+    }
     newtBody.id = "tBodyMainExplorer";
     newtBody.innerHTML = insert;
-    
     if(lastInside !== newtBody.innerHTML){
         gebid("tBodyMainExplorer")?.remove();
-        lastInside = newtBody.innerHTML;
         gebid("fileShower")?.appendChild(newtBody);
     }
+    lastInside = newtBody.innerHTML;
 }
 
 function updateTime() {
@@ -184,24 +217,30 @@ function findNodeById(tree, targetId){
     return null; 
 }
 
-function createFolderNesting(depth, max){
+function createFolderNesting(depth, max, parentID){
+    globalIdCounter++;
     if (max <= 0 || depth > 4) return null;
+    var currentThisFolderID = globalIdCounter; 
     var inside = []; 
+    
     let randomForFor = (getRandomNumber(max/2) + 0.5 * max);
     for(let i = 0; i < randomForFor; i++){ 
-        const childFolder = createFolderNesting(depth + 1, max - 1);
+        const childFolder = createFolderNesting(depth + 1, max - 1, currentThisFolderID);
         if (childFolder) inside.push(childFolder);
     }
+    
     var addedFileSize = 1;
     for(var i=0; i< inside.length; i++){
         addedFileSize += (inside[i]["fileSize"] || 1);
     }
+    
     return {
         Name: randomFolderList[getRandomNumber(randomFolderList.length)], 
         inside: inside, 
         depth: depth,
-        id: globalIdCounter++,
-        fileSize: addedFileSize
+        id: currentThisFolderID,
+        fileSize: addedFileSize,
+        parentID: parentID
     };
 }
 
@@ -209,12 +248,22 @@ function createTree(maxDepth){
     globalIdCounter = 0;
     var out = [{Name: "User", inside: [], depth:0, id: -1}];
     for(let i = 0; i < maxDepth; i++){ 
-        var out2 = createFolderNesting(1, 3);
+        var out2 = createFolderNesting(1, 3, -1);
         if(out2) out[0]["inside"].push(out2);
     }
     return out;
 }
-
+function startSpeedrun(time){
+    timeLeft = time;
+    setInterval(tickDownTimer, 1000)
+    if(timeLeft <= 0){
+        gameOver();
+    }
+}
+var timeLeft;
+function tickDownTimer(){
+    timeLeft--;
+}
 document.addEventListener("DOMContentLoaded", () => {
     var xButtons = document.querySelectorAll(".closeMenusX");
     xButtons.forEach(function(button) {
